2fa0c969d744098c2c63997bbcc082cc6d826614
Sivanandham <siva24anand@gmail.com>
2019-01-24 15:13:53 +0530

    featured comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
+import { FeaturedComponent } from './featured/featured.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    FeaturedComponent
   ],
   imports: [
     BrowserModule,
